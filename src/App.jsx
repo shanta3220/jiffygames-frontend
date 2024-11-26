8640940bdd00b2ef5617b78141e4e1409f66b43d
NJShanta <njshanta.ca@gmail.com>
2024-11-26 04:46:20 -0500

    Add responsive header and background

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,68 @@
 import { useState } from "react";
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.scss";
+import Header from "./components/Header/Header";
 
 function App() {
-  const [count, setCount] = useState(0);
-
-  return <></>;
+  return (
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<HomePage />}></Route>
+        <Route path="/games" element={<GamesPage />}></Route>{" "}
+        <Route path="/leaderboards" element={<LeaderboardsPage />}></Route>
+        <Route path="/register" element={<RegisterPage />}></Route>
+        <Route path="/login" element={<LoginPage />}></Route>
+        <Route path="/user-profile" element={<UserProfilePage />}></Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
+
+function HomePage() {
+  return (
+    <div>
+      <p>This is HomePage</p>
+    </div>
+  );
+}
+
+function GamesPage() {
+  return (
+    <div>
+      <p>This is GamesPage</p>
+    </div>
+  );
+}
+
+function LeaderboardsPage() {
+  return (
+    <div>
+      <p>This is LeaderboardsPage</p>
+    </div>
+  );
+}
+
+function RegisterPage() {
+  return (
+    <div>
+      <p>This is RegisterPage</p>
+    </div>
+  );
+}
+function LoginPage() {
+  return (
+    <div>
+      <p>This is HomePage</p>
+    </div>
+  );
+}
+function UserProfilePage() {
+  return (
+    <div>
+      <p>This is UserProfilePage</p>
+    </div>
+  );
+}
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,56 @@
+import "./Header.scss";
+import { Link, NavLink } from "react-router-dom";
+import logoIcon from "/logo.png";
+
+function Header() {
+  const isLoggedIn = false;
+
+  return (
+    <div className="header">
+      <div className="header__logo">
+        <img
+          src={logoIcon}
+          alt="JiffyGames logo"
+          className="header__logo-icon"
+        />
+        <p className="header__logo-title">
+          JIFFY<span className="header__logo-title--Games">GAMES</span>
+        </p>
+      </div>
+      <div className="header__links">
+        <NavLink to={"/"} className="header__link">
+          HOME
+        </NavLink>
+        <NavLink to={"/games"} className="header__link">
+          GAMES
+        </NavLink>
+        <NavLink to={"/leaderboards"} className="header__link">
+          LEADERBOARDS
+        </NavLink>
+      </div>
+      <div className="header__links">
+        {!isLoggedIn && (
+          <NavLink to={"/register"} className="header__link">
+            LOGIN/SIGN UP
+          </NavLink>
+        )}
+        {isLoggedIn && (
+          <>
+            <Link to={"/"} className="header__link">
+              LOGOUT
+            </Link>{" "}
+            <Link to={"/user-profile"} className="header__user-profile">
+              <img
+                src={"../../assets/images/user-profile.png"}
+                alt="User Avatar"
+                className="user-avatar"
+              />
+            </Link>
+          </>
+        )}
+      </div>
+    </div>
+  );
+}
+
+export default Header;
